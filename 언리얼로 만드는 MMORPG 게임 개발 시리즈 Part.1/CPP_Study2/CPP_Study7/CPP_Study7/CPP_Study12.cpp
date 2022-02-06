15b0ea149dc9c5d4afc184977cbd88130a259444
Luen4773 <89366919+Luen4773@users.noreply.github.com>
2022-02-06 18:27:38 +0900

    2022.02.06
    Update0.3

--- 언리얼로 만드는 MMORPG 게임 개발 시리즈 Part.1/CPP_Study2/CPP_Study7/CPP_Study7/CPP_Study12.cpp
+++ 언리얼로 만드는 MMORPG 게임 개발 시리즈 Part.1/CPP_Study2/CPP_Study7/CPP_Study7/CPP_Study12.cpp
@@ -1,58 +1,58 @@
-#include <iostream>
-using namespace std;
-
-
-// 오늘의 주제 : 함수(프로시저 , 메소드, 루틴)
-
-
-/*
-
-input 으로 무엇을 받고, output으로 무엇을 뱉을지 정해준다.
-반환타입 함수이름()
-{
-
-		함수내용 
-
-		return ~~~;
-
-
-*/
-
-//Hello World를 콘솔에 출력하는 함수를 만들어보자!
-//input : 없음 / output : 없음
-//타입 : int float double char~~
-
-void PrintHelloWorld() {
-	
-	cout << "Hello World" << endl;
-
-	return;
-}
-
-void PrintNumber(int number)
-{
-	cout << " 넘겨 주신 숫자는 " << number << "입니다 " << endl;
-}
-
-
-// 2를 곱하고 결과물을 뱉는 함수 만들어보자
-// input : int // output : int
-
-int MultiplyBy2(int a, int b)
-{
-	int c = a * b;
-	return c;
-}
-
-int main() {
-	
-	int d = 3;
-	int f = 3;
-
-	int a = MultiplyBy2(d, f);
-
-	PrintNumber(a);
-
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//
+//// 오늘의 주제 : 함수(프로시저 , 메소드, 루틴)
+//
+//
+///*
+//
+//input 으로 무엇을 받고, output으로 무엇을 뱉을지 정해준다.
+//반환타입 함수이름()
+//{
+//
+//		함수내용 
+//
+//		return ~~~;
+//
+//
+//*/
+//
+////Hello World를 콘솔에 출력하는 함수를 만들어보자!
+////input : 없음 / output : 없음
+////타입 : int float double char~~
+//
+//void PrintHelloWorld() {
+//	
+//	cout << "Hello World" << endl;
+//
+//	return;
+//}
+//
+//void PrintNumber(int number)
+//{
+//	cout << " 넘겨 주신 숫자는 " << number << "입니다 " << endl;
+//}
+//
+//
+//// 2를 곱하고 결과물을 뱉는 함수 만들어보자
+//// input : int // output : int
+//
+//int MultiplyBy2(int a, int b)
+//{
+//	int c = a * b;
+//	return c;
+//}
+//
+//int main() {
+//	
+//	int d = 3;
+//	int f = 3;
+//
+//	int a = MultiplyBy2(d, f);
+//
+//	PrintNumber(a);
+//
+//
+//	return 0;
+//}
