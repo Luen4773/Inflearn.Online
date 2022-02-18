b0bf033fe23d6ba37f738d73ee56b23ac6e9f6c4
Luen4773 <89366919+Luen4773@users.noreply.github.com>
2022-02-18 19:39:56 +0900

    2022.02.18 Update

--- 언리얼로 만드는 MMORPG 게임 개발 시리즈 Part.1/CPP_Study2/CPP_Study7/CPP_Study7/CPP_Study13.cpp
+++ 언리얼로 만드는 MMORPG 게임 개발 시리즈 Part.1/CPP_Study2/CPP_Study7/CPP_Study7/CPP_Study13.cpp
@@ -1,45 +1,45 @@
-#include <iostream>
-
-using namespace std;
-
-// 오늘의 주제 : 지역 변수와 값 전달
-
-// 전역 변수 
-// 참고) 초기화 여부 : const 등 .rotato .data. bss
-
-int globalValue = 0;
-
-
-void Test() {
-
-	globalValue++;
-
-}
-
-
-void increaseHp(int hp)
-{
-	int finalHp = hp + 1;
-	//hp = hp + 1;
-}
-
-// 스택
-// [[매개변수][RET][지역변수(hp 1)][매개변수(hp=1)][RET][지역변수 finalHp]             ]
-// 스택은 끝나면 다시 돌아오면서 제거하면서 돌아온다.
-int main() {
-
-
-	//지역 변수
-
-
-	int hp = 1;
-
-	cout << "increase 호출 전" << hp << endl;
-	increaseHp(hp);
-	cout << "increase 호출 후 " << hp << endl;
-
-	int LocalValue = 0;
-
-
-	return 0;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//// 오늘의 주제 : 지역 변수와 값 전달
+//
+//// 전역 변수 
+//// 참고) 초기화 여부 : const 등 .rotato .data. bss
+//
+//int globalValue = 0;
+//
+//
+//void Test() {
+//
+//	globalValue++;
+//
+//}
+//
+//
+//void increaseHp(int hp)
+//{
+//	int finalHp = hp + 1;
+//	//hp = hp + 1;
+//}
+//
+//// 스택
+//// [[매개변수][RET][지역변수(hp 1)][매개변수(hp=1)][RET][지역변수 finalHp]             ]
+//// 스택은 끝나면 다시 돌아오면서 제거하면서 돌아온다.
+//int main() {
+//
+//
+//	//지역 변수
+//
+//
+//	int hp = 1;
+//
+//	cout << "increase 호출 전" << hp << endl;
+//	increaseHp(hp);
+//	cout << "increase 호출 후 " << hp << endl;
+//
+//	int LocalValue = 0;
+//
+//
+//	return 0;
+//}
